e54c2675e7e5b83870a0ecf2fdab2e71bad53f1c
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-04-04 10:55:22 -0300

    Add new file

---
+++ practica0-3.hs
@@ -0,0 +1,13 @@
+{-
+3) Definir una función que determine si un año es bisiesto o no, de
+acuerdo a la siguiente definición:
+
+año bisiesto 1. m. El que tiene un día más que el año común, añadido al mes de febrero. Se repite
+cada cuatro años, a excepción del último de cada siglo cuyo número de centenas no sea múltiplo
+de cuatro. (Diccionario de la Real Academia Espaola, 22ª ed.)
+
+¿Cuál es el tipo de la función definida?
+-}
+
+bisiesto a = (mod a 4 == 0) && not (mod a 100 == 0) || (mod a 400 == 0)
+
