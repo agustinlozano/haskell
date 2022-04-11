12c2ad0a358e65415f91fa6838e5a25a13844c5f
Agustin Lozano Blua <agustinn.lozanoo@hotmail.com>
2022-04-11 10:05:06 -0300

    Update file

--- practice-2/1-rgb.hs
+++ practice-2/1-rgb.hs
@@ -12,34 +12,3 @@ type Color = (Float, Float, Float)
  
 mezclar :: Color -> Color -> Color
 mezclar (r1, g1, b1) (r2, g2, b2) = ( (r1 + r2)/2 , (g1 + g2)/2, (b1 + b2)/2 )
-
-
-
--- 2)
-type Linea = (Int, [Char])
-
-vacia :: Linea
-vacia = (0, []) 
-
-moverIzq :: Linea -> Linea
-moverIzq (p, xs)
-    | p /= 0    = (p-1, xs)
-    | otherwise = (p, xs)
-
-moverDer :: Linea -> Linea
-moverDer (p, xs)
-    | p /= length xs = (p+1, xs)
-    | otherwise      = (p, xs)
-
-moverIni :: Linea -> Linea
-moverIni (p, xs) = (0, xs)
-
-moverFin :: Linea -> Linea
-moverFin (p, xs) = (length xs, xs)
-
-insertar :: Char -> Linea -> Linea
-insertar c (p, xs) = (p+1, (take p xs) ++ [c] ++ (drop p xs))
-
-borrar :: Linea -> Linea
-borrar (0, xs) = (0, xs)
-borrar (p, xs) = (p-1, (take (p-1) xs) ++ (drop p xs))
