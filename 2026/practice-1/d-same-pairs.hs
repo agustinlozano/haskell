31a15cc120deb0a90546f82b22a710a5e1377186
Agustin L. B. <agustinlozanoblua@gmail.com>
2026-03-26 16:47:17 -0300

    Add description for `paresIguales` function

---
+++ 2026/practice-1/d-same-pairs.hs
@@ -0,0 +1,11 @@
+{-
+    d) paresIguales :: Int → Int → Int → Int → Bool
+
+    toma 4 numeros enteros y retorna True si de dos en dos son iguales (en cualquier orden), 
+    en los demas casos retorna False. Por ejemplo:
+
+    paresIguales 3 1 1 2 = False 
+    paresIguales 3 1 3 1 = True 
+    paresIguales 3 3 1 1 = True
+    paresIguales 3 1 1 3 = True
+-}
