e6b472c72eeb3684ed6552feaca0c698ad932c27
Agustin L. B. <agustinlozanoblua@gmail.com>
2026-03-26 16:29:28 -0300

    Add helper functions for collecting key-value pairs

--- 2026/practice-1/b-collect-key-value.hs
+++ 2026/practice-1/b-collect-key-value.hs
@@ -1,7 +1,8 @@
--- collect :: [(k, v)] -> [(k, [v])]
--- collect [] = []
-
+-- helper para poder resolver `collect`
 myInsert k v [] = [(k,[v])]
 myInsert k v ((k',vs):xs) =
   if k == k' then (k,v:vs) : xs
   else (k',vs) : myInsert k v xs
+
+collect [] = []
+collect ((k,v):xs) = myInsert k v (collect xs)
