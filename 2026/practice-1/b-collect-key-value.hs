0320993d574a4532609b401d06c17a2e3013fc88
Agustin L. B. <agustinlozanoblua@gmail.com>
2026-03-26 16:46:50 -0300

    Refactor myInsert function to use guards for clarity

--- 2026/practice-1/b-collect-key-value.hs
+++ 2026/practice-1/b-collect-key-value.hs
@@ -1,8 +1,8 @@
 -- helper para poder resolver `collect`
 myInsert k v [] = [(k,[v])]
-myInsert k v ((k',vs):xs) =
-  if k == k' then (k,v:vs) : xs
-  else (k',vs) : myInsert k v xs
+myInsert k v ((k',vs):xs)
+    | k == k' = (k,v:vs) : xs
+    | otherwise = (k',vs) : myInsert k v xs
 
 collect [] = []
 collect ((k,v):xs) = myInsert k v (collect xs)
