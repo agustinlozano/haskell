438635242adea0c8e4d14bc20555501827e4a933
Agustin L. B. <agustinlozanoblua@gmail.com>
2026-03-28 19:48:00 -0300

    Improve documentation for helper functions and rename `paresIguales` to `samePairs`

--- 2026/practice-1/d-same-pairs.hs
+++ 2026/practice-1/d-same-pairs.hs
@@ -1,6 +1,7 @@
 {-
     Helper `contains`
-    Dada una lista y un elemento verifica si esta contenido
+    Dada una lista y un elemento verifica si 
+    el elemento esta contenido en la lista
 -}
 
 contains :: Eq a => a -> [a] -> Bool
@@ -11,7 +12,7 @@ contains n (x:xs)
 
 {-
     Helper `uniques`
-    Dada una lista devuleve otra con los valores unicos
+    Dada una lista devuleve otra con los valores unicos (sin duplicados)
 -}
 
 uniques :: Eq a => [a] -> [a]
@@ -24,7 +25,8 @@ uniques xs = f xs []
 
 {-
     Helper `frequency`
-    Dado un elemento y una lista, devulve la cantidad de veces que aparece el elementos
+    Dado un elemento y una lista, devulve la cantidad de veces 
+    que aparece el elemento en la lista
 -}
 
 frequency :: Eq a => a -> [a] -> Int
@@ -39,12 +41,9 @@ frequency e xs = f xs 0
 {-
     Helper `frequencies`
     Dada una lista mapea otra donde cada elemento es una tupla (v,f)
-    Donde
-        v es un valor unico en lista
+    Donde:
+        v es un valor unico en lista original
         f es la frecuencia del valor en la misma
-    
-    Tipo de la funcion deberia ser algo asi:
-    
 -}
 
 frequencies :: Eq a => [a] -> [(a,Int)]
@@ -52,17 +51,29 @@ frequencies [] = []
 frequencies xs = map (\x -> (x,frequency x xs)) (uniques xs)
 
 {-
-    d) paresIguales :: Int → Int → Int → Int → Bool
+    d) samePairs :: Int → Int → Int → Int → Bool
 
     toma 4 numeros enteros y retorna True si de dos en dos son iguales (en cualquier orden), 
     en los demas casos retorna False. Por ejemplo:
 
-    paresIguales 3 1 1 2 = False 
-    paresIguales 3 1 3 1 = True 
-    paresIguales 3 3 1 1 = True
-    paresIguales 3 1 1 3 = True
+    samePairs 3 1 1 2 = False 
+    samePairs 3 1 3 1 = True 
+    samePairs 3 3 1 1 = True
+    samePairs 3 1 1 3 = True
 -}
 
+{-
+    Mi solución
+    Convertir un problema de combinaciones en uno de frecuencias
+
+    Para eso:
+    - Tratar los valores como una lista
+    - Averiguar los valores únicos en la lista original
+    - Averigual la frecuencia con la que aparece cada valor unico en la lista original
+    - Finalmente, verificar que se cumplan las condificiones del ejercicio
+-}
+
+samePairs :: Eq a => a -> a -> a -> a -> Bool
 samePairs a b c d = (length freqs == 2) && (all validFreq freqs)
     where
         xs = [a,b,c,d]
