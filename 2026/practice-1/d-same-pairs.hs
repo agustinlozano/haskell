902e4af8c219a60c5ab3f4a98b5f589e1f7cf8a4
Agustin L. B. <agustinlozanoblua@gmail.com>
2026-03-28 18:19:49 -0300

    Add helper functions: contains and uniques

--- 2026/practice-1/d-same-pairs.hs
+++ 2026/practice-1/d-same-pairs.hs
@@ -1,4 +1,42 @@
 {-
+    Helper `contains`
+    Dada una lista y un elemento verifica si esta contenido
+-}
+
+contains :: Eq a => a -> [a] -> Bool
+contains _ [] = False
+contains n (x:xs)
+    | n == x = True
+    | otherwise = contains n xs
+
+{-
+    Helper `uniques`
+    Dada una lista devuleve otra con los valores unicos
+-}
+
+uniques :: Eq a => [a] -> [a]
+uniques xs = f xs []
+    where
+        f [] acc = acc
+        f (x:xs) acc
+            | contains x xs = f xs acc
+            | otherwise = f xs (x : acc)
+
+{-
+    Helper `frequencies`
+    Dada una lista mapea una tupla (v,f)
+    Donde
+        v es un valor unico en lista
+        f es la frecuencia del valor en la misma
+    
+    Tipo de la funcion deberia ser algo asi:
+    frequencies :: (Num b) [a] -> (a,b) 
+-}
+
+-- frequencies [] = []
+-- frequencies xs = map someFn xs
+
+{-
     d) paresIguales :: Int → Int → Int → Int → Bool
 
     toma 4 numeros enteros y retorna True si de dos en dos son iguales (en cualquier orden), 
@@ -9,3 +47,9 @@
     paresIguales 3 3 1 1 = True
     paresIguales 3 1 1 3 = True
 -}
+
+-- paresIguales a b c d = resultado
+--   where
+--     xs = [a,b,c,d]
+--     valores = ...
+--     frecs = ...
