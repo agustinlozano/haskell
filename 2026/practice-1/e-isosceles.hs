450e0a6b5c191902e634419992afdb0346022b5f
Agustin L. B. <agustinlozanoblua@gmail.com>
2026-03-28 20:47:46 -0300

    Implement isosceles triangle check with validation for side lengths

---
+++ 2026/practice-1/e-isosceles.hs
@@ -0,0 +1,21 @@
+{-
+    e) isosceles :: Int → Int → Int → Bool 
+    que dadas la longitud de los lados de un triangulo nos dice si es un triangulo isosceles
+-}
+
+badIsosceles :: Int -> Int -> Int -> Bool
+badIsosceles a b c
+    | a == b = True
+    | b == c = True
+    | c == a = True
+    | otherwise = False
+
+-- La solucion de arriba esta mal porque no corrobora que
+-- La suma de ambos lados iguales sean mayor en long al tercel lado
+
+isosceles :: Int -> Int -> Int -> Bool
+isosceles a b c
+    | a == b && a + b > c = True
+    | b == c && b + c > a = True
+    | a == c && a + c > b = True
+    | otherwise = False
