65fc317c95fbf029a628f2247beed7ba0e91248a
Agustin L. B. <agustinlozanoblua@gmail.com>
2026-03-29 01:33:10 -0300

    Make it a comment to avoid error GHC-44432

--- 2026/practice-1/c-generic-type-for-serie.hs
+++ 2026/practice-1/c-generic-type-for-serie.hs
@@ -1,5 +1,6 @@
 -- c) serie que se comporta de la siguiente manera: 
 -- serie [1, 2, 3] = [[ ], [1], [1, 2], [1, 2, 3]] 
--- Dar su tipo mas general
+-- Dar su tipo mas general:
 
-serie :: (Num a) => [a] -> [[a]] 
+-- serie :: (Num a) => [a] -> [[a]]
+
