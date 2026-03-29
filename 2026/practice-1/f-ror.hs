c794c061a25a63bad8627936671274a1d3822c26
Agustin L. B. <agustinlozanoblua@gmail.com>
2026-03-29 02:38:32 -0300

    Refactor `pushFoward` function and improve algorithm efficiency in `ror`

--- 2026/practice-1/f-ror.hs
+++ 2026/practice-1/f-ror.hs
@@ -1,8 +1,11 @@
--- Primer try usando el helper `pushForward`
-pushFoward _ [] = []
-pushFoward n ((i,v):xs) 
-    | i > n-1 = v : pushFoward n xs
-    | otherwise = pushFoward n xs ++ [v]
+-- First try using `pushForward` helper
+pushFoward1 _ [] = []
+pushFoward1 n ((i,v):xs)
+    | i > n-1 = v : pushFoward1 n xs
+    | otherwise = pushFoward1 n xs ++ [v]
+
+-- The issue here happens bc ++ operator is executed each iteration
+-- Due the operator has O(n) complexity (linear growth) that aint good ^^
 
 {-
     f) ror 
@@ -14,5 +17,17 @@ pushFoward n ((i,v):xs)
 
 ror1 _ [] = []
 ror1 n xs 
+    | n > length xs = []
+    | otherwise = pushFoward1 n (zip [0..] xs)
+
+-- 2th try making the algorith faster
+-- This approach only use ++ operator once 🧐
+
+pushFoward _ [] = []
+pushFoward n items = 
+    [v | (i, v) <- items, i > n-1] ++ [v | (i, v) <- items, i <= n-1]
+
+ror _ [] = []
+ror n xs 
     | n > length xs = []
     | otherwise = pushFoward n (zip [0..] xs)
