e30ca3af24038094dd692ed14c9003222049fd66
Agustin L. B. <agustinlozanoblua@gmail.com>
2026-03-29 22:58:03 -0300

    Add initial description of the 'upto' function

---
+++ 2026/practice-1/g-upto.hs
@@ -0,0 +1,11 @@
+{-
+    g) upto :: Int → Int → [Int] 
+    Que dado dos numeros enteros n y m devuelve la lista:
+    [n, n + 1, n + 2, ..., m ]
+
+    - caso n <= m por un lado
+    - la lista [ ] por otro
+
+    No usar listas por comprension.
+-}
+
