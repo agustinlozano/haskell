a515da4641701fd0fd2a9c204ef3426835590fb7
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2026-04-15 21:12:50 -0300

    Solve practice-2 editor challenge

---
+++ 2026/practice-2/2-editor.hs
@@ -0,0 +1,28 @@
+type Linea = (Int, [Char])
+
+vacia :: Linea
+vacia = (0, [])
+
+moverIzq :: Linea -> Linea
+moverIzq (p, chars) 
+    | p > 0     = (p - 1, chars)
+    | otherwise = (p, chars)
+
+moverDer :: Linea -> Linea
+moverDer (p, chars)
+    | p < length chars = (p + 1, chars)
+    | otherwise        = (p, chars)
+
+moverIni :: Linea -> Linea
+moverIni (p, chars) = (0, chars)
+
+moverFin :: Linea -> Linea
+moverFin (p, chars) = (length chars, chars)
+
+insertar :: Char -> Linea -> Linea
+insertar c (p, chars) =
+    (p+1, (take p chars) ++ [c] ++ (drop p chars))
+
+borrar :: Linea -> Linea
+borrar (p, chars) =
+    (p-1, (take p chars) ++ (drop (p+1) chars))
