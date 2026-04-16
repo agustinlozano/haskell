5b61ba4f669ddb6bb62c60e4bceca35458b22a5d
Agustin L. B. <agustinlozanoblua@gmail.com>
2026-04-16 13:47:53 -0300

    Add 'divisores' and 'abundantes' functions to calculate abundant numbers

---
+++ 2026/practice-1/2-c-abundantes.hs
@@ -0,0 +1,18 @@
+{-
+    Helper to return a list of divisors
+-}
+
+divisores n = [m | m <- [1..(n-1)], n `mod` m == 0]
+
+{-
+    c) abundantes :: [Integer] 
+    
+    Lista de todos los numeros abundantes. 
+    Un numero natural n se denomina abundante si es menor que la suma de sus divisores propios. 
+    Por ejemplo, 12 y 30 son abundantes pero 5 y 28 no lo son.
+    
+    Por ejemplo abundates = [12, 18, 20, 24, 30, 36, ...]
+-}
+
+abundantes :: [Integer]
+abundantes = [n | n <- [0..100], n < sum (divisores n)]
