21168a2a1f8f480a0f64d3e6233464aa727509ce
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2026-04-15 21:49:17 -0300

    Add deleteLast function to remove the last element from a list

---
+++ 2026/practice-1/a-delete-last-one.hs
@@ -0,0 +1,10 @@
+{-
+    borrarLast 
+    Que dada una lista borra el ultimo elemento de la misma. 
+    No utilizar reverse, ni tail.
+-}
+
+deleteLast :: [a] -> [a]
+deleteLast [] = []
+deleteLast [x] = []
+deleteLast (x:xs) = x : deleteLast xs
