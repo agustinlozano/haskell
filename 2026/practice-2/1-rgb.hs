4aa1f4e2d9254b41a35c5a39af2b45634814ca48
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2026-04-15 21:12:12 -0300

    Add Color data type and mezclar function for RGB color mixing

---
+++ 2026/practice-2/1-rgb.hs
@@ -0,0 +1,5 @@
+data Color = RGB Int Int Int
+    deriving (Show)
+
+mezclar :: Color -> Color -> Color
+mezclar (RGB r1 v1 a1) (RGB r2 v2 a2) = RGB (div (r1 + r2) 2) (div (v1 + v2) 2) (div (a1 + a2) 2)
