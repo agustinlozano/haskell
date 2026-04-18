d515264c656c5903e4b2eb2aae201db8250fdb01
Agustin B. Lozano <agustinlozanoblua@gmail.com>
2026-04-18 18:36:00 -0300

    Refactor collect and myInsert functions for clarity and consistency

--- 2026/practice-1/1-b-collect-key-value.hs
+++ 2026/practice-1/1-b-collect-key-value.hs
@@ -1,8 +1,10 @@
 -- helper para poder resolver `collect`
+myInsert :: Eq t => t -> t1 -> [(t, [t1])] -> [(t, [t1])]
 myInsert k v [] = [(k,[v])]
 myInsert k v ((k',vs):xs)
     | k == k' = (k,v:vs) : xs
     | otherwise = (k',vs) : myInsert k v xs
 
+collect :: Eq t => [(t, t1)] -> [(t, [t1])]
 collect [] = []
 collect ((k,v):xs) = myInsert k v (collect xs)
